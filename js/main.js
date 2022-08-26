3eb57f4238042e340218b6731fed9ceacb32da47
yorjanrafa <66923631+yorjanrafa@users.noreply.github.com>
2022-08-25 20:28:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,97 @@
+import { data } from "./data.js";
+
+console.log(data);
 let trend = document.querySelector("#trend");
 let price = document.querySelector("#price");
 
-console.log()
+let parent = document.querySelector("table");
 
-const up = "⬆⬆⬆";
-const down = "⬇⬇⬇";
+function createNode(origin, price) {
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let b   = document.createElement("b");
+  let td2 = document.createElement("td");
 
-if (trend.textContent === up) {
-  trend.classList.add("trend-up");
+  b.appendChild(document.createTextNode(origin))
+  td2.appendChild(document.createTextNode(price))
+  td1.appendChild(b)
+  tr.appendChild(td1)
+  tr.appendChild(td2)
+  return tr;
 }
-if (trend.textContent === down) {
-  trend.classList.add("trend-down");
-}
 
-trend.textContent = "Bajando 👇👇🏻";
+let sites = {
+  "enparalelovzla":function () { return createNode(this.id, this.format.rate)},
+  "bcv": function () { return createNode(this.id, this.format.rate)},
+  "monitor-dolar": function () { return createNode(this.id, this.format.rate)},
+  "banco-de-venezuela": function () { return createNode(this.id, this.format.rate)},
+  "dolartoday": function () { return createNode(this.id, this.format.rate)},
+  "banco-mercantil": function () { return createNode(this.id, this.format.rate)},
+  "banco-bbva-provincial": function () { return createNode(this.id, this.format.rate)},
+  "banco-banesco": function () { return createNode(this.id, this.format.rate)},
+  "monedero-paypal": function () { return createNode(this.id, this.format.rate)},
+  "reserve": function () { return createNode(this.id, this.format.rate)},
+} 
 
 async function apiDollarFetch() {
-  return await fetch("https://s3.amazonaws.com/dolartoday/data.json")
+  return await fetch(
+    "https://exchangemonitor.net/api/ve?user=YORJAN_200822&token=JHtygv65g5j8w0fy1DoP&currency=USD&filter-group"
+  )
     .then((res) => res.json())
-    .then(response => (price.textContent = response.USD.sicad1))// convertir a json
+    .then((response) =>
+      response.data.forEach((e) => {
+        console.log(response)
+        if (sites[e.id]){ 
+        parent.appendChild(createNode(e.id, e.format.rate));
+
+}})
+    ) // convertir a json
     .catch((err) => console.log("Solicitud fallida", err)); // Capturar errores
 }
 
-let datajson =  apiDollarFetch()
-console.log(datajson)
-setTimeout(function(){
-    console.log(datajson['USD']);
-}, 2000);
+apiDollarFetch()
+// let datajson = apiDollarFetch();
+// console.log(datajson);
+// setTimeout(function () {
+//   console.log(datajson["USD"]);
+// }, 2000);
 
+// let parent = document.querySelector("table");
+
+// function createNode(origin, price) {
+//   let tr = document.createElement("tr");
+//   let td1 = document.createElement("td");
+//   let b   = document.createElement("b");
+//   let td2 = document.createElement("td");
+
+//   b.appendChild(document.createTextNode(origin))
+//   td2.appendChild(document.createTextNode(price))
+//   td1.appendChild(b)
+//   tr.appendChild(td1)
+//   tr.appendChild(td2)
+//   return tr;
+// }
+
+// data.forEach((e) => {
+//   parent.appendChild(createNode(e.id, e.format.rate));
+// });
+
+// let parent = document.querySelector("table");
+
+// function createNode(origin, price) {
+//   let tr = document.createElement("tr");
+//   let td1 = document.createElement("td");
+//   let b   = document.createElement("b");
+//   let td2 = document.createElement("td");
+
+//   b.appendChild(document.createTextNode(origin))
+//   td2.appendChild(document.createTextNode(price))
+//   td1.appendChild(b)
+//   tr.appendChild(td1)
+//   tr.appendChild(td2)
+//   return tr;
+// }
+
+// data.forEach((e) => {
+//   parent.appendChild(createNode(e.id, e.format.rate));
+// });
